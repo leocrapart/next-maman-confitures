afb897eeb5d26fd0f1a77f2289a0f19da1e60b0e
leolecrado <leocrapart@yahoo.fr>
2020-10-06 14:02:28 +0200

    1

---
+++ pages/info.js
@@ -0,0 +1,13 @@
+import Nav from '../components/nav'
+import Background from '../components/bg'
+
+export default function InfoPage() {
+
+    return (
+        <div>
+            <Nav></Nav>
+            <div>info page</div>
+            <Background></Background>
+        </div>
+    )
+}
